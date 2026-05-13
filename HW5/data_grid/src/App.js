4561ceddbe3574eff795481cabfe45c04213b40e
B1329061 <cgu1329061@gmail.com>
2026-05-13 15:11:11 +0800

    Add files via upload

---
+++ HW5/data_grid/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CLOCK from './clock';
+
+function App() {
+  return (
+    <div className="App">
+      <CLOCK />
+    </div>
+  );
+}
+
+export default App;
---
+++ HW5/data_grid/src/HelloCGU.js
@@ -0,0 +1,8 @@
+// src/HelloCGU.js
+import React from 'react';
+
+// 定言一個大寫開頭的組件 (Component)
+function HelloCGU() {
+    return <h1>Hello CGU</h1>;
+}
+export default HelloCGU;
